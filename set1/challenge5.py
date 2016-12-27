46f6538c62557f34b6de92867f80adfac39191f3
Alan Gou <alanjgou@gmail.com>
2016-12-27 09:41:06 +0800

    Finished challenge 5

---
+++ set1/challenge5.py
@@ -0,0 +1,40 @@
+# https://cryptopals.com/sets/1/challenges/5
+# Implement repeating-key XOR
+# Here is the opening stanza of an important work of the English language:
+
+# Burning 'em, if you ain't quick and nimble
+# I go crazy when I hear a cymbal
+# Encrypt it, under the key "ICE", using repeating-key XOR.
+
+# In repeating-key XOR, you'll sequentially apply each byte of the key; the first byte of plaintext will be XOR'd against I, the next C, the next E, then I again for the 4th byte, and so on.
+
+# It should come out to:
+
+# 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
+# a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+# Encrypt a bunch of stuff using your repeating-key XOR function. Encrypt your mail. Encrypt your password file. Your .sig file. Get a feel for it. I promise, we aren't wasting your time with this.
+import binascii
+
+ANSWER = b"0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272\
+a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+def encrypt_with_key(key, message):
+    key_length = len(key)
+    output_list = []
+    for index, byte in enumerate(message):
+        key_index = index % key_length
+        output_list.append(byte ^ key[key_index])
+    output_bytes = bytes(output_list)
+    return output_bytes
+
+def gen(n):
+    return b"ICE"
+
+def main():
+    message = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    result = encrypt_with_key(gen(3), message)
+    assert binascii.hexlify(result) == ANSWER
+
+if __name__ == "__main__":
+    main()
+
