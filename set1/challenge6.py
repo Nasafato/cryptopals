99b60b69141c32328c73c578916e85c7c5bb2186
Alan Gou <alanjgou@gmail.com>
2016-12-29 20:15:14 +0800

    Got the transpose blocks and padding blocks working

--- set1/challenge6.py
+++ set1/challenge6.py
@@ -2,6 +2,7 @@
 # The big daddy of Set 1
 from challenge1 import get_bit_in_byte
 import pprint
+import array
 from collections import namedtuple
 
 LikelyKeysize = namedtuple("LikelyKeysize", "size normalizedDistance")
@@ -48,24 +49,49 @@ def get_likeliest_keysizes(data, number=1):
     results.sort(key=lambda x: x.normalizedDistance)
     return results[:number]
 
+def get_padded_block(extra_bytes, keysize):
+    null_bytes_needed = keysize - len(extra_bytes)
+    null_bytes = array.array('b', [0] * null_bytes_needed)
+    return extra_bytes + bytes(null_bytes)
+
+
 def breakup_blocks(data, keysize):
     blocks = []
-    index = 0
     extra_bytes = len(data) % keysize
-    while index < len(data):
+
+    prev_index = 0
+    index = keysize
+    while index <= len(data):
+        blocks.append(data[prev_index:index])
         prev_index = index
         index += keysize
-        blocks.append(data[prev_index:index])
 
-    # make sure the last block is equal to the last block of the data
-    if extra_bytes == 0:
+    if extra_bytes > 0:
+        padded_block = get_padded_block(data[prev_index:], keysize)
+        blocks.append(padded_block)
+        assert len(blocks) == len(data) // keysize + 1
+    else:
+        # make sure the last block is equal to the last block of the data
+        assert len(blocks) == len(data) // keysize
         assert blocks[-1] == data[-keysize:]
 
-    print("{} blocks: {} extra bytes: data is {} bytes on keysize {}".format(len(blocks), extra_bytes, len(data), keysize))
-    return [bytes(b'asdf')]
+    # print("{} blocks: {} extra bytes: data is {} bytes on keysize {}".format(len(blocks), extra_bytes, len(data), keysize))
+    return blocks
 
 def transpose_blocks(blocks, keysize):
-    return [bytes(b'asdf')]
+    transposed_blocks = []
+    for i in range(keysize):
+        new_block = []
+        for block in blocks:
+            new_block.append(block[i])
+        transposed_blocks.append(bytes(new_block))
+
+    assert len(transposed_blocks) == keysize
+    block_length = len(transposed_blocks[0])
+    for block in transposed_blocks:
+        assert block_length == len(block)
+
+    return transposed_blocks
 
 def get_single_byte_keys(tranposed_blocks):
     return b'asdf'
@@ -74,7 +100,7 @@ def get_single_byte_keys(tranposed_blocks):
 def get_likeliest_keys(data, potential_keysizes):
     keys = []
     pprint.pprint(potential_keysizes)
-    for result in potential_keysizes[:1]:
+    for result in potential_keysizes:
         keysize = result.size
         blocks = breakup_blocks(data, keysize)
         transposed_blocks = transpose_blocks(blocks, keysize)
